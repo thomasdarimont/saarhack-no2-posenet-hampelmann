1ad324c9ec5fd0e14fa7421e537dc678f7e7c946
Thomas Darimont <thomas.darimont@googlemail.com>
2019-10-19 17:17:41 +0200

    Try to show noseimage

--- google-coral-pi/run_hampelmann_camera.py
+++ google-coral-pi/run_hampelmann_camera.py
@@ -70,7 +70,7 @@ def draw_pose(dwg, pose, color='yellow', threshold=0.2):
         dwg.add(dwg.circle(center=(xysNose[0], xysNose[1]), r=int(dxEars/3.0),
                                fill='red', fill_opacity=1.0, stroke=color))
 
-        noseImage = SVGImage("../media/Nose.png", (xysNose[0], xysNose[1]),(64, 64))
+        noseImage = SVGImage("file:///../media/Nose.png", (xysNose[0], xysNose[1]),(64, 64))
         dwg.add(noseImage)
 
     for a, b in EDGES:
